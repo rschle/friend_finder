231ce47d2a48c921e2d1690bf48574c1d978c89b
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-06-29 17:44:06 -0700

    difference stuff getting better

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -5,73 +5,71 @@ module.exports = (app) => {
   const path = require("path");
 
 
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-  
-    app.get("/api/friends", (req, res) => {
-      res.json(friends);
-    });
-  
-  
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // ---------------------------------------------------------------------------
-  
-    app.post("/api/friends", (req, res) => {
-      // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-      // It will do this by sending out the value "true" have a table
-      // req.body is available since we're using the body parsing middleware
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
 
-      var userData = req.body;
-      var userAnswers = userData.scores;
+  app.get("/api/friends", (req, res) => {
+    res.json(friends);
+  });
 
-      //parsing out the scores from the new user's answers so that they are inegers
-      var userAnsInt = [];
-      for(var i = 0; i < userAnswers.length; i++){
-        userAnsInt.push(parseInt(userAnswers[i]));
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", (req, res) => {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+
+    var userData = req.body;
+    var userAnswers = userData.scores;
+
+    //setting initial conditions so that we can get smallest difference and make 
+    //that the match later on
+    var mainDiff = 1000000;
+    var matchName = "";
+    var matchPhoto = "";
+    //parsing out the scores from the new user's answers so that they are inegers
+    // var userAnsInt = [];
+    // for (var i = 0; i < userAnswers.length; i++) {
+    //   userAnsInt.push(parseInt(userAnswers[i]));
+    // }
+
+    friends.push(userData);
+    res.json(true);
+    // console.log(userAnswers);
+    // console.log(userAnsInt);
+
+    for (var i = 0; i < friends.length - 1; i++) {
+      var diff = 0;
+      for (var j = 0; j < userAnswers.length; j++) {
+        diff += Math.abs(friends[i].scores[j] - userAnswers[j]);
+      };
+      console.log(diff);
+
+      if (diff < mainDiff) {
+        mainDiff = diff;
+        matchName = friends[i].name;
+        matchPhoto = friends[i].photo;
       }
 
-          friends.push(userData);
-          res.json(true);
-      // console.log(userAnswers);
-      console.log(userAnsInt);
+    }
+    console.log(matchName);
+    console.log(matchPhoto);
 
-      for(var i = 0; i < friends.length; i++){
-        var diff = 0;
-        for(var j = 0; j < userAnsInt.length; j++){
-            diff += Math.abs(friends[i].scores[j] - userAnsInt[j]);
-        };
-        console.log(diff);
+  });
 
 
-      }
-      // console.log(parseInt(friends[0].scores))
+  // app.post("/api/clear", (req, res) => {
+  //   // Empty out the arrays of data
+  //   friends.length = 0;
+  //   res.json({ ok: true });
 
-      // for(var i = 0; i < friends.length; i++){
-      //   var totaldiff = 0
-      //   var userScoreArray = [];
-      //   for(var j = 0; j < 10; j++){
-      //     userScoreArray.push(parseInt(friends[i].scores[j]))
-      //     totaldiff += Math.abs(friends[i].userScoreArray[j] - userAnsInt[j]);
-      //   }
-       
-      //   console.log(userScoreArray);
-      //   console.log(totaldiff);
-      // }
-      
-
-    });
-  
-  
-    // app.post("/api/clear", (req, res) => {
-    //   // Empty out the arrays of data
-    //   friends.length = 0;
-    //   res.json({ ok: true });
-
-    // });
-  };
+  // });
+};
--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -10,6 +10,7 @@ module.exports = (app) => {
     app.get("/survey", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
+    
 }
 
 
